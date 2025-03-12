90e0fe2f8305c8e0ca8946f6664b1f6d1339a6da
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-12 18:21:34 +0100

    Work in progress

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,60 @@
+import { categories } from "../data/categories";
+import { use, useState } from "react";
+
 export default function Form() {
-  return (
-    <div>Form</div>
-  )
+  const [activity, setActivity] = useState({
+    category: '',
+    name: '',
+    calories: 0
+  })
+    return (
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">
+          Categoría:
+        </label>
+        <select
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+          value={activity.category}
+        >
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="name" className="font-bold">
+          Actividad:
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. Comida, Ensalada, Ejercicio, Pesas, Bicicleta"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">
+          Calorias:
+        </label>
+        <input
+          type="number"
+          id="calories"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. 20, 50, 100"
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value="Guardar comida o guardar ejercicio"
+      />
+    </form>
+  );
 }
