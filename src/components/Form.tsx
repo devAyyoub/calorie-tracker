56c11542f2e18debe01eed457a11f8ebec00e6f4
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-13 22:22:33 +0100

    generated unique ID's for each activiy

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
-import { categories } from "../data/categories";
 import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { categories } from "../data/categories";
 import type { Activity } from "../types";
 import { ActivityActions } from "../redecurs/activity-reducer";
 
@@ -7,12 +8,15 @@ type FormProps = {
   dispatch: Dispatch<ActivityActions>;
 };
 
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: "",
+  calories: 0,
+};
+
 export default function Form({ dispatch }: FormProps) {
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: "",
-    calories: 0,
-  });
+  const [activity, setActivity] = useState<Activity>(initialState);
 
   const handleChange = (
     e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
@@ -34,7 +38,10 @@ export default function Form({ dispatch }: FormProps) {
 
     dispatch({ type: "save-activity", payload: { newActivity: activity } });
 
-    setActivity({ category: 1, name: "", calories: 0 });
+    setActivity({
+        ...initialState,
+        id: uuidv4()
+    });
   };
   return (
     <form
