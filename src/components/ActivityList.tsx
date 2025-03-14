d0fe7d4f6b11ac3e317e6f87535d09f6a182ca23
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-14 19:22:15 +0100

    added edit button

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import type { Activity } from "../types";
 import { categories } from "../data/categories";
 import { useMemo } from "react";
-
+import { PencilSquareIcon } from "@heroicons/react/16/solid";
 type ActivityListProps = {
   activities: Activity[];
 };
@@ -39,7 +39,11 @@ export default function ActivityList({ activities }: ActivityListProps) {
               <span>Calorias</span>
             </p>
           </div>
-          <div></div>
+          <div className="flex gap-5 items-center">
+            <button>
+              <PencilSquareIcon className="h-8 w-8 text-gray-800" />
+            </button>
+          </div>
         </div>
       ))}
     </>
