60b57456c5e72a6a90f52cb2d506376879133969
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-16 19:12:34 +0100

    added action to restart the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,46 @@
-import { useReducer, useEffect } from 'react'
-import Form from './components/Form'
-import { activityReducer, initialState } from './redecurs/activity-reducer'
-import ActivityList from './components/ActivityList'
+import { useReducer, useEffect, useMemo } from "react";
+import Form from "./components/Form";
+import { activityReducer, initialState } from "./redecurs/activity-reducer";
+import ActivityList from "./components/ActivityList";
 
 function App() {
-
-  const [state, dispatch] = useReducer(activityReducer, initialState)
+  const [state, dispatch] = useReducer(activityReducer, initialState);
 
   useEffect(() => {
-    localStorage.setItem('activities', JSON.stringify(state.activities))
-  }, [state.activities])
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities]);
+
+  const canRestartApp = () =>
+    useMemo(() => state.activities.length, [state.activities]);
 
   return (
     <>
-      <header className='bg-lime-600 py-3'>
-        <div className='max-w-4xl mx-auto flex justify-between'>
-          <h1 className='text-center text-lg font-bold text-white uppercase'>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de Calorias
           </h1>
+
+          <button
+            className="bg-gray-800 hover:bg-gray-900 text-white p-2 font-bold uppercase enabled:cursor-pointer rounded-lg text-sm disabled:opacity-10"
+            disabled={!canRestartApp()}
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
+            Reiniciar App
+          </button>
         </div>
       </header>
-      <section className='bg-lime-500 py-20 px-5'>
-        <div className='max-w-4xl mx-auto'>
-          <Form
-            dispatch={dispatch}
-            state={state}
-          />
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form dispatch={dispatch} state={state} />
         </div>
       </section>
 
-      <section className='p-10 mx-auto max-w-4xl'>
-        <ActivityList
-          activities={state.activities}
-          dispatch={dispatch}
-        />
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList activities={state.activities} dispatch={dispatch} />
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/redecurs/activity-reducer.ts
+++ src/redecurs/activity-reducer.ts
@@ -1,23 +1,24 @@
 import type { Activity } from "../types";
 
-export type ActivityActions = 
-{ type: "save-activity", payload: { newActivity: Activity } } |
-{ type: "set-activeId", payload: { id: Activity['id'] } } |
-{ type: "delete-activity", payload: { id: Activity['id'] } } 
+export type ActivityActions =
+  | { type: "save-activity"; payload: { newActivity: Activity } }
+  | { type: "set-activeId"; payload: { id: Activity["id"] } }
+  | { type: "delete-activity"; payload: { id: Activity["id"] } }
+  | { type: "restart-app" };
 
 export type ActivityState = {
   activities: Activity[];
-  activeId: Activity['id']
+  activeId: Activity["id"];
 };
 
-const localStorageActivities = () : Activity[] => {
-    const activities = localStorage.getItem('activities')
-    return activities ? JSON.parse(activities) : []
-} 
+const localStorageActivities = (): Activity[] => {
+  const activities = localStorage.getItem("activities");
+  return activities ? JSON.parse(activities) : [];
+};
 
 export const initialState: ActivityState = {
   activities: localStorageActivities(),
-  activeId : ''
+  activeId: "",
 };
 
 export const activityReducer = (
@@ -25,31 +26,41 @@ export const activityReducer = (
   action: ActivityActions
 ) => {
   if (action.type === "save-activity") {
-
-    let updatedActivies : Activity[] = []
-    if(state.activeId) {
-        updatedActivies = state.activities.map(activity => activity.id === state.activeId ? action.payload.newActivity : activity)
+    let updatedActivies: Activity[] = [];
+    if (state.activeId) {
+      updatedActivies = state.activities.map((activity) =>
+        activity.id === state.activeId ? action.payload.newActivity : activity
+      );
     } else {
-        updatedActivies = [...state.activities, action.payload.newActivity]
+      updatedActivies = [...state.activities, action.payload.newActivity];
     }
     return {
-        ...state,
-        activities: updatedActivies,
-        activeId : ''
-    }
+      ...state,
+      activities: updatedActivies,
+      activeId: "",
+    };
   }
 
-  if(action.type === 'set-activeId') {
+  if (action.type === "set-activeId") {
     return {
-        ...state,
-        activeId: action.payload.id
-    }
+      ...state,
+      activeId: action.payload.id,
+    };
   }
 
-  if(action.type === 'delete-activity') {
+  if (action.type === "delete-activity") {
     return {
-        ...state,
-        activities: state.activities.filter(activity => activity.id !== action.payload.id)
+      ...state,
+      activities: state.activities.filter(
+        (activity) => activity.id !== action.payload.id
+      ),
+    };
+  }
+
+  if (action.type === 'restart-app') {
+    return {
+        activities: [],
+        activeId: '',
     }
   }
 
