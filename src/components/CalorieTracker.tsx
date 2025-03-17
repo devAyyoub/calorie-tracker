2863ffbff09b215278d76cc09a412e0a42578d2d
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-17 17:54:01 +0100

    Created component to show the total of calories, added logic to show the consumed calories and also the burned ones

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,11 +1,56 @@
-import { Activity } from "../types"
+import { useMemo } from "react";
+import { Activity } from "../types";
 
 type CalorieTrackerProps = {
-    activities: Activity[]
-}
+  activities: Activity[];
+};
 
-export default function CalorieTracker({activities}: CalorieTrackerProps) {
+export default function CalorieTracker({ activities }: CalorieTrackerProps) {
+  const caloriesConsumed = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+
+  const caloriesBurned = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 2 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
   return (
-    <div>CalorieTracker</div>
-  )
+    <>
+      <h2 className="text-4xl font-black text-white text-center">
+        Resumen de calorias
+      </h2>
+
+      <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-5 ">
+        <p className="text-white font-bold  rounded-full grid grid-cols-1 gap-3 text-center">
+          <span className="font-black text-6xl text-orange">
+            {caloriesConsumed}
+          </span>
+          Consumidas
+        </p>
+        <p className="text-white font-bold  rounded-full grid grid-cols-1 gap-3 text-center">
+          <span className="font-black text-6xl text-orange">
+            {caloriesBurned}
+          </span>
+          Ganadas
+        </p>
+        <p className="text-white font-bold  rounded-full grid grid-cols-1 gap-3 text-center">
+          <span className="font-black text-6xl text-orange">
+            
+          </span>
+          Consumidas
+        </p>
+      </div>
+    </>
+  );
 }
