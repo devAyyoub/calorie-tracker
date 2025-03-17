17d87c1c2e44ace5dddc4c97d71ea24a7806f2c2
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-03-17 18:20:23 +0100

    added netCalories which is the difference between consumed calories and burned calories

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import { Activity } from "../types";
+import CalorieDisplay from "./CalorieDisplay";
 
 type CalorieTrackerProps = {
   activities: Activity[];
@@ -25,31 +26,27 @@ export default function CalorieTracker({ activities }: CalorieTrackerProps) {
       ),
     [activities]
   );
+
+  const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [activities])
   return (
     <>
       <h2 className="text-4xl font-black text-white text-center">
         Resumen de calorias
       </h2>
 
-      <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-5 ">
-        <p className="text-white font-bold  rounded-full grid grid-cols-1 gap-3 text-center">
-          <span className="font-black text-6xl text-orange">
-            {caloriesConsumed}
-          </span>
-          Consumidas
-        </p>
-        <p className="text-white font-bold  rounded-full grid grid-cols-1 gap-3 text-center">
-          <span className="font-black text-6xl text-orange">
-            {caloriesBurned}
-          </span>
-          Ganadas
-        </p>
-        <p className="text-white font-bold  rounded-full grid grid-cols-1 gap-3 text-center">
-          <span className="font-black text-6xl text-orange">
-            
-          </span>
-          Consumidas
-        </p>
+      <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-5 ">       
+        <CalorieDisplay
+            calories={caloriesConsumed}
+            text= "Consumidas"
+        />
+        <CalorieDisplay
+            calories={caloriesBurned}
+            text= "Ejercicio"
+        />
+        <CalorieDisplay
+            calories={netCalories}
+            text= "Diferencia "
+        />
       </div>
     </>
   );
